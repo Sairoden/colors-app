288007da8923601703868e2d172f7d194f92749e
Sairoden <odingandarosa@gmail.com>
2022-01-05 18:35:56 +0800

    Add Link to Palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -35,6 +35,10 @@ const styles = {
 };
 
 class PaletteList extends Component {
+  goToPalette(id) {
+    this.props.history.push(`/palette/${id}`);
+  }
+
   render() {
     const { palettes, classes } = this.props;
 
@@ -46,7 +50,12 @@ class PaletteList extends Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
-              <MiniPalette {...palette} />
+              <Link to={`/palette/${palette.id}`}>
+                <MiniPalette
+                  {...palette}
+                  handleClick={() => this.goToPalette(palette.id)}
+                />
+              </Link>
             ))}
           </div>
         </div>
