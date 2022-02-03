216837aec9656b79fa0b16e79317b112db48f396
Sairoden <odingandarosa@gmail.com>
2022-02-03 17:58:32 +0800

    Fix FormDialog and install Emoji Picker

--- src/PaletteMetaForm.js
+++ src/PaletteMetaForm.js
@@ -6,6 +6,8 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
 
 class PaletteMetaForm extends Component {
   constructor(props) {
@@ -31,26 +33,24 @@ class PaletteMetaForm extends Component {
     });
   }
 
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    this.setState({ open: false });
-  };
-
   render() {
     const { newPaletteName } = this.state;
+    const { hideForm, handleSubmit } = this.props;
 
     return (
-      <Dialog open={this.state.open} onClose={this.handleClose}>
+      <Dialog
+        open={this.state.open}
+        aria-labelledby="form-dialog-title"
+        onClose={hideForm}
+      >
         <DialogTitle>Choose a Palette Name</DialogTitle>
-        <ValidatorForm onSubmit={() => this.props.handleSubmit(newPaletteName)}>
+        <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
           <DialogContent>
             <DialogContentText>
               Please enter a name for your new beautiful palette. Make sure it's
               unique!
             </DialogContentText>
+            <Picker />
             <TextValidator
               label="Palette Name"
               value={newPaletteName}
@@ -63,7 +63,7 @@ class PaletteMetaForm extends Component {
             />
           </DialogContent>
           <DialogActions>
-            <Button onClick={this.handleClose}>Cancel</Button>
+            <Button onClick={hideForm}>Cancel</Button>
             <Button variant="contained" color="primary" type="submit">
               Save Palette
             </Button>
