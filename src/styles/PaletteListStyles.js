fe2344f38c2e2c78f708e1c9ceb313fd531eb54e
Sairoden <odingandarosa@gmail.com>
2022-02-08 14:22:44 +0800

    Make PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from "./sizes.js";
+
 export default {
   root: {
     height: "100vh",
@@ -13,6 +15,12 @@ export default {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [sizes.down("xl")]: {
+      width: "80%",
+    },
+    [sizes.down("xs")]: {
+      width: "75%",
+    },
   },
   nav: {
     display: "flex",
@@ -30,5 +38,12 @@ export default {
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
     gridGap: "2.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1rem",
+    },
   },
 };
